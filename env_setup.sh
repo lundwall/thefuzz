76ba82175f768f09a0a92b6c8321abbd657cc7eb
lundwall <23060984+lundwall@users.noreply.github.com>
2023-06-06 16:50:24 +0200

    Fix env_setup.sh bug

--- env_setup.sh
+++ env_setup.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
-if [ $REPRODUCE == "lineinfile" ]
+if [ "$REPRODUCE" == "lineinfile" ]
   then
     ## We are reproducing the "./" bug in lineinfile
     ## First we must revert the code that fixes the issue in lineinfile.py
     echo "reproducing lineinfile bug"
     bash /mnt/reproduce_lineinfile.sh
-elif [ $REPRODUCE == "rhsm" ]
+elif [ "$REPRODUCE" == "rhsm" ]
   then
     ## We are reproducing the locale bug in rhsm
     ## First we must revert the code that fixes the issue in rhsm_repository.py
